768c89f0fad258a169afab287d07726313b43742
hammad1985 <hammad.yaqoob1985@yahoo.co.uk>
2018-07-14 16:15:55 +0100

    fixed bug fixed html page

--- src/main/java/com/guru/springframework/chucknorrisjockapp/controller/JokeController.java
+++ src/main/java/com/guru/springframework/chucknorrisjockapp/controller/JokeController.java
@@ -15,7 +15,7 @@ public class JokeController {
 
     @RequestMapping({"/",""})
     public String showJoke(Model model) {
-        model.addAttribute("Joke", jokeService.getJoke());
+        model.addAttribute("joke", jokeService.getJoke());
         return "chucknorris";
     }
 }
